0519f77594023ba2f0f3662b50cdcc763050fa45
Henrik Bernström <henrik.bernstrom@jayway.com>
2016-09-02 12:34:21 +0200

    Fixed graphical glitches. Added conversation status and session date.

--- web/app/scripts/controllers/session.js
+++ web/app/scripts/controllers/session.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('SessionCtrl', function ($scope, $stateParams, $timeout, $sce, $firebaseArray, $firebaseObject, $state, sessionsService) {
+app.controller('SessionCtrl', function ($scope, $stateParams, $http, $timeout, $sce, $firebaseArray, $firebaseObject, $state, sessionsService, localStorageService) {
 
   $scope.loading = true;
   $scope.trustSrc = function(src) {
@@ -11,8 +11,21 @@ app.controller('SessionCtrl', function ($scope, $stateParams, $timeout, $sce, $f
     $scope.loading = false;
     $scope.sessions = sessions;
     var sessionRef = $scope.sessions.$ref().child($stateParams.sessionId);
-    $scope.session = $firebaseObject(sessionRef);
+    $firebaseObject(sessionRef).$loaded().then(function (session) {
+      $scope.session = session;
+      loadConversationData();
+    });
   });
+
+  var loadConversationData = function () {
+    $http.get('https://api.yammer.com/api/v1/messages/in_group/' + $scope.session.yammerGroupId + '.json',
+      {headers: {'Authorization': 'Bearer ' + localStorageService.get("yammer_access_token")}}).
+      success(function (response) {
+        $scope.session.groupConversationCount = response.messages.length;
+        var participants = _.where(response.references, {type: 'user'});
+        $scope.session.participantsCount = participants.length;
+      });
+  };
 
   $scope.$watch('session', function () {
     yam.connect.embedFeed({
--- web/app/scripts/controllers/sessions.js
+++ web/app/scripts/controllers/sessions.js
@@ -1,22 +1,34 @@
 'use strict';
 
-app.controller('SessionsCtrl', function ($scope, sessionsService) {
+app.controller('SessionsCtrl', function ($scope, $timeout, $http, sessionsService, localStorageService) {
   $scope.loading = true;
   sessionsService.getSessions().$loaded().then(function (sessions) {
     $scope.sessions = sessions;
+    loadConversationData();
     $scope.loading = false;
   });
   $scope.showHints = true;
 
   var tabs = [
-      {title: 'Webinar', type: 'Webinar'},
-      {title: 'Panels', type: 'Panel'},
-      {title: 'Round Tables', type: 'Round Tables'},
-      {title: 'Q&A\'s', type: 'Q&A\s'},
-      {title: 'Others', type: 'Others'},
-      {title: 'All content', type: ''}
-    ];
+    {title: 'Webinar', type: 'Webinar'},
+    {title: 'Panels', type: 'Panel'},
+    {title: 'Round Tables', type: 'Round Tables'},
+    {title: 'Q&A\'s', type: 'Q&A\s'},
+    {title: 'Others', type: 'Others'},
+    {title: 'All content', type: ''}
+  ];
   $scope.tabs = tabs;
   $scope.selectedIndex = 5;
 
+  var loadConversationData = function () {
+    _.each($scope.sessions, function (session, index) {
+      $http.get('https://api.yammer.com/api/v1/messages/in_group/' + session.yammerGroupId + '.json',
+        {headers: {'Authorization': 'Bearer ' + localStorageService.get("yammer_access_token")}}).
+        success(function (response) {
+          $scope.sessions[index].groupConversationCount = response.messages.length;
+          var participants = _.where(response.references, {type: 'user'});
+          $scope.sessions[index].participantsCount = participants.length;
+        });
+    });
+  };
 });
